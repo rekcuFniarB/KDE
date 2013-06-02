da6e62864ea0f90714fad7eea57cf73d4459d55e
BrainFucker <retratserif@gmail.com>
2013-06-02 20:55:39 +0400

    Moved sources from src/ for fixing building issues for launchpad.

--- Utils/Imageshack.us/iconsgen.sh
+++ Utils/Imageshack.us/iconsgen.sh
@@ -4,7 +4,9 @@
 ## Getting the icon
 ## wget 'http://upload.wikimedia.org/wikipedia/en/f/f9/Imageshacknew.png' -O "$1/ImageShack.png"
 
-uudecode -o "$2/ImageShack.png"
+## $1: source dir, $2: binary dir.
+
+uudecode -o "$2/ImageShack.png" "$1/ImageShack.png.uu"
 
 Sizes="64 48 32 24 22 16"
 echo "Making:"
@@ -13,6 +15,6 @@ do
     Dir="$2/icons/hicolor/${x}x${x}/apps"
     mkdir -p "$Dir"
     echo $x
-    convert "$1/ImageShack.png" -resize ${x}x${x}\! "$Dir/ImageShack.png"
+    convert "$2/ImageShack.png" -resize ${x}x${x}\! "$Dir/ImageShack.png"
 done
 echo "Generated icons."

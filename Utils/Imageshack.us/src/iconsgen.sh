ce981f2e44252a8e1c71fb078d5425f9317bb7fa
BrainFucker <retratserif@gmail.com>
2013-06-02 15:37:51 +0400

    Added logo file and updated CMakeLists.txt because wget fails on Launchpad.net when building Resolving upload.wikimedia.org (upload.wikimedia.org)... failed: Name or service not known. wget: unable to resolve host address upload.wikimedia.org

--- Utils/Imageshack.us/src/iconsgen.sh
+++ Utils/Imageshack.us/src/iconsgen.sh
@@ -2,15 +2,15 @@
 # Generate icons
 
 ## Getting the icon
-wget 'http://upload.wikimedia.org/wikipedia/en/f/f9/Imageshacknew.png' -O "$1/ImageShack.png"
+# wget 'http://upload.wikimedia.org/wikipedia/en/f/f9/Imageshacknew.png' -O "$1/ImageShack.png"
 
 Sizes="64 48 32 24 22 16"
 echo "Making:"
 for x in $Sizes
 do
-    Dir="$1/icons/hicolor/${x}x${x}/apps"
+    Dir="$2/icons/hicolor/${x}x${x}/apps"
     mkdir -p "$Dir"
     echo $x
-    convert "$1/ImageShack.png" -resize ${x}x${x}\! "$Dir/ImageShack.png"
+    convert "$1/Frog-White-Lipped-small.png" -resize ${x}x${x}\! "$Dir/ImageShack.png"
 done
 echo "Generated icons."

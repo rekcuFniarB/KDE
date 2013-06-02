24a2def69ba9e04ee9361e9011c22911c7b8b92d
BrainFucker <retratserif@gmail.com>
2013-06-02 08:17:11 +0400

    Removed temporary directory .tmp.

--- Utils/Imageshack.us/.tmp/CMakeFiles/imageshack.us.bin.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/imageshack.us.bin"
-  "imageshack.us"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/imageshack.us.bin.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- Utils/Imageshack.us/.tmp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/brainfucker/Projects/KDE/Utils/Imageshack.us
-# Build directory: /home/brainfucker/Projects/KDE/Utils/Imageshack.us/.tmp
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
